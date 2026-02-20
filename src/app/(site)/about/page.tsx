b16ab2d1dd8505bf431b872b4ce6e8eb144f5596
David <samkwesidavid456@gmail.com>
2026-02-20 08:17:05 +0000

    update

--- src/app/(site)/about/page.tsx
+++ src/app/(site)/about/page.tsx
@@ -253,7 +253,7 @@ function Person({
             src={img}
             alt={name}
             fill
-            className="object-cover object-[50%_20%]"
+            className="object-cover object-[50%_47%]"
           />
         </div>
       </div>
