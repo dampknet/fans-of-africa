752c468df0cf359d96be0fa7bae148efb5417e03
David <samkwesidavid456@gmail.com>
2026-02-23 12:37:03 +0000

    update stemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ export default function sitemap() {
     { url: "https://fansofafrica.com", lastModified: new Date() },
     { url: "https://fansofafrica.com/about", lastModified: new Date() },
     { url: "https://fansofafrica.com/projects", lastModified: new Date() },
-    { url: "https://fansofafrica.com/volunteers", lastModified: new Date() },
+    // { url: "https://fansofafrica.com/volunteers", lastModified: new Date() },
     { url: "https://fansofafrica.com/contact", lastModified: new Date() },
     { url: "https://fansofafrica.com/donate", lastModified: new Date() },
   ];
