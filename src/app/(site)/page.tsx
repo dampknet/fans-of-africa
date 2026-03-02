0ac499cf87ce80f6c0f2c54504ce8dea7d3e1b30
David <samkwesidavid456@gmail.com>
2026-03-02 11:11:15 +0000

    update

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -124,7 +124,7 @@ export default function HomePage() {
                 label="Schools Impacted"
                 value={[
                   "Mt. Moriah Academy — Kumasi",
-                  "Candit Standard School — Bibiani",
+                  "Kandit Standard School — Bibiani",
                   "Terry International Christian Academy (T.I.C.A.) — Abuesi",
                 ]}
               />
